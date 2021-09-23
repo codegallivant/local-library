8338b0f6687f2a1804e6b0a64931a13e00ce2431
EpicThunderZ <janak3.1415et@gmail.com>
2021-09-24 04:03:39 +0530

    v0.9.8

--- ui/get-books.js
+++ ui/get-books.js
@@ -1,7 +1,6 @@
 function loadBooks(identityId, identityName, checkEmpty) {
     "use strict";
     var request = new XMLHttpRequest();
-<<<<<<< HEAD
     request.onreadystatechange = function() {
         if (request.readyState === XMLHttpRequest.DONE) {
             if (identityId === "" && identityName === "") {
@@ -11,24 +10,12 @@ function loadBooks(identityId, identityName, checkEmpty) {
             }
             if (document.getElementById("bookIdBox").value.length === 0 && document.getElementById("bookNameBox").value.length === 0 && checkEmpty !== false) {
                 loadBooks("", "");
-=======
-    request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE) {
-            if(identityId==="" && identityName==="") {
-              checkEmpty=false;
-            } else {
-              checkEmpty=true;
-            }
-            if(document.getElementById("bookIdBox").value.length===0 && document.getElementById("bookNameBox").value.length===0 && checkEmpty!==false) {
-              loadBooks("", "");
->>>>>>> f05836a67c278646085fa9c873dab62e09cb7554
             }
             var books = document.getElementById('book-display');
             if (request.status === 200) {
                 var content = ` `;
                 var status;
                 var bookData = JSON.parse(this.responseText);
-<<<<<<< HEAD
                 for (var i = 0; i < bookData.length; i++) {
                     if (bookData[i].QuantityLeft === 0) {
                         status = "danger";
@@ -40,24 +27,11 @@ function loadBooks(identityId, identityName, checkEmpty) {
                     content += `
 
                       <tr class="bg-${status}" >
-=======
-                for (var i=0; i< bookData.length; i++) {
-                    if(bookData[i].QuantityLeft===0) {
-                      status="danger";
-                    } else if(bookData[i].Issued!==0) {
-                      status="warning";
-                    } else {
-                      status="success";
-                    }
-                    content += `
-                      <tr class="bg-${status}">
->>>>>>> f05836a67c278646085fa9c873dab62e09cb7554
                         <td>${bookData[i].id}</td>
                         <td>${bookData[i].BookName}</td>
                         <td>${bookData[i].Issued}</td>
                         <td>${bookData[i].TotalQuantity}</td>
                         <td>${bookData[i].QuantityLeft}</td>
-<<<<<<< HEAD
                         <td><button class = "btn btn-outline-light" onclick = "bookInfo(${bookData[i].id}, '${bookData[i].BookName}', ${bookData[i].Issued}, ${bookData[i].TotalQuantity}, ${bookData[i].QuantityLeft},'${status}');">View details</td>
                       </tr>
 
@@ -171,42 +145,3 @@ var bookInfo = function(bookID, bookName, Issued, TotalQuantity, QuantityLeft, s
         identityId: bookID
     }));
 }
-=======
-                      </tr>
-
-                    `;
-                }
-   /* <HR>
-
-                        <div class="commentBoxPar" id="${programData[i].tag}_CommentBoxPar">
-                            <TEXTAREA id="${programData[i].tag}_commentBox" style="overflow:auto; width: 70%; height: 100%; max-width: 70%; max-height: 100%; min-width: 70%; min-height: 100%;" class="commentBox"> </TEXTAREA>
-                        </div>
-
-                        <div class="commentDiv" id="${programData[i].tag}_CommentDiv">
-                        <div class="commentB" id="${programData[i].tag}_CommentB"><button>Comment</button></div>
-                        <div class="ShowCom" id="${programData[i].tag}_ShowCom"><button>Show All Comments</button></div>
-                        </div>
-                        </div>*/
-                console.log(content);
-                books.innerHTML = content;
-            } else {
-                books.innerHTML='Oops! Could not load all books!';
-            }
-        }
-    };
-    console.log(document.getElementById("bookIdBox", '').value);
-    request.open('POST', '/get-books', true);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify({identityId:identityId ,identityName:identityName}));
-};
-
-document.getElementById("bookIdBox").addEventListener("input", function() {
-  loadBooks(document.getElementById("bookIdBox").value);
-});
-document.getElementById("bookNameBox").addEventListener("input", function() {
-  loadBooks("", document.getElementById("bookNameBox").value);
-});
-if(document.getElementById("bookIdBox").value.length===0 && document.getElementById("bookNameBox").value.length===0) {
-  loadBooks("", "");
-}
->>>>>>> f05836a67c278646085fa9c873dab62e09cb7554
